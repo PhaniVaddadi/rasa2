2cb97ac4f1b76039e7dc5ac207387a7ff08e3170
phaniv11 <62028807+phaniv11@users.noreply.github.com>
2020-06-30 17:43:23 +0530

    Add files via upload

---
+++ flask/request.py
@@ -0,0 +1,24 @@
+from flask import Flask,jsonify,request,make_response
+import requests
+from flask_restful import Api
+from flask_cors import CORS
+import json
+
+app=Flask(__name__)
+api = Api(app)
+CORS(app)
+
+@app.route('/',methods=['GET', 'POST']) #end point is chat
+
+def post():
+	json1 = request.get_json() #it will take the request in json format and storing in json
+	# data = json.dumps(({"sender": "Rasa","message":"hi"}))
+	print("JSON Data: {}".format(json1))
+	json.dumps(json1)
+	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+	r = requests.post("http://localhost:5005/webhooks/rest/webhook", json=json1,headers=headers) #geting the response and stored in r by using the data in json
+	return r.json()[0] 
+
+
+if __name__ == '__main__':
+	app.run(debug=True) #adi endpoint adi nenu 
