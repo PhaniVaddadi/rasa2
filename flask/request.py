813d448d1c5cda055d68101b1fb978f2c90e6a89
phaniv11 <62028807+phaniv11@users.noreply.github.com>
2020-07-10 14:44:44 +0530

    Update request.py

--- flask/request.py
+++ flask/request.py
@@ -1,24 +1,29 @@
-from flask import Flask,jsonify,request,make_response
-import requests
-from flask_restful import Api
+from flask import Flask, render_template, request, jsonify, redirect
 from flask_cors import CORS
+import requests
 import json
 
-app=Flask(__name__)
-api = Api(app)
+app = Flask(__name__)
 CORS(app)
 
-@app.route('/',methods=['GET', 'POST']) #end point is chat
+@app.route('/index',methods=["GET"])
+def hello_world():
+	return render_template('index.html')
 
+@app.route('/',methods=["POST"])
 def post():
-	json1 = request.get_json() #it will take the request in json format and storing in json
+	try:
+		user_message = request.get_json()
+		# user_message = json.dumps(user_message)
 
-	print("JSON Data: {}".format(json1))
-	json.dumps(json1)
-	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-	r = requests.post("http://rasa_server:5005/webhooks/rest/webhook", json=json1,headers=headers) #geting the response and stored in r by using the data in json
-	return r.json()[0] 
-
+		print("User messge: {}".format(user_message))
+		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+		response = requests.post("http://rasa_server:5005/webhooks/rest/webhook", json = user_message, headers = headers)
+		print("Something filled1111")
+		return jsonify({"status":"success","response":json.loads(response.content.decode())})
+	except Exception as e:
+		print(e)
+		return jsonify({"status":"success","response":"Sorry I am not trained to do that yet..."})
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
