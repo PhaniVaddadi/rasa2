b990240f5691dcfcf28b6ed60fc2268c2d4be725
phaniv11 <62028807+phaniv11@users.noreply.github.com>
2020-07-23 16:18:31 +0530

    Update request.py

--- flask/request.py
+++ flask/request.py
@@ -6,24 +6,25 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/',methods=["GET", "POST"])
+@app.route('/',methods=[ "GET"])
 def home():
-    if request.method == 'POST':
-        try:
-            user_message = request.get_json()
-            # user_message = json.dumps(user_message)
-
-            print("User messge: {}".format(user_message))
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            response = requests.post("http://rasa_server:5005/webhooks/rest/webhook", json = user_message, headers = headers)
-            print("Something filled1111")
-            return jsonify({"status":"success","response":json.loads(response.content.decode())})
-        except Exception as e:
-            print(e)
-            return jsonify({"status":"success","response":"Sorry I am not trained to do that yet..."})       
-
-
     return render_template('index.html')
 
+@app.route('/',methods=[ "POST"])
+def chat():
+    try:
+        user_message = request.get_json()
+        print("User messge: {}".format(user_message))
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        response = requests.post("http://rasa_server:5005/webhooks/rest/webhook", json = user_message, headers = headers)
+        print("Something filled1111")
+        print(response)
+        r = response.json()
+        print(r)
+        return jsonify(r)
+    except Exception as e:
+        print(e)
+        return jsonify({"status":"success","response":"Sorry I am not trained to do that yet..."})       
+
 if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0')
+        app.run(debug=True)
